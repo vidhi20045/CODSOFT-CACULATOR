1b06117f00f58c89882481b04ba0fda1d31ccea6
unknown <24avyanna@gmail.com>
2024-02-14 22:21:58 +0530

    CALCULATOR

---
+++ calculator.js
@@ -0,0 +1,18 @@
+// /calcuator/
+const display= document.getElementById("display");
+function appendtodisplay(input){
+    display.value +=input;
+
+}
+function cleardisplay(){
+display.value="";
+
+}
+function calculate(){
+    try{
+display.value=eval(display.value);
+    }
+    catch(error){
+        display.value="ERROR";
+    }
+}
